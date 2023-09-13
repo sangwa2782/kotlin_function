dbbf74b0ca3fb7db6e07893fc153bdd860494e5f
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-13 12:20:14 +0530

    takes an array of integers and prints only even numbers.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
+
+// takes an array of integers and prints only even numbers.
+
 fun main(args: Array<String>) {
-    val name = "Ajay Kumar"
-    welcomUser(name)
+   val numbers = intArrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+    printEvenNumber(numbers)
 }
 
-fun welcomUser(name : String) {
-    println("Hello, $name We welcome you.")
+fun printEvenNumber(numbers: IntArray) {
+  println("Even Numbers: ")
+    for (number in numbers) {
+        if (number % 2 == 0) {
+            println(number)
+        }
+    }
 }
