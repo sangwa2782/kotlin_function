c49662157c804c7f8acb652c8ceb577d7bea0978
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-15 19:47:25 +0530

    perform addition, subtraction, multiplication and division of two numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,44 @@ import java.util.Date
 import java.util.Scanner
 
 
-// Write a Kotlin program to display the current date and time.
+// Write a Kotlin program to perform addition, subtraction, multiplication and division of two numbers.
 
 fun main () {
-    dateandTime()
+    val s = Scanner(System.`in`)
+
+    print("Enter First digit : ")
+    val a = s.nextInt()
+
+    print("Enter Second digit : ")
+    val b = s.nextInt()
+
+    val addResult = addition(a,b)
+    println("On Adding $a and $b, we have result = $addResult")
+
+    val subResult = subtract(a,b)
+    println("On Subtracting $a and $b, we have result = $subResult")
+
+    val mulResult = multiply(a,b)
+    println("On Multiplying $a and $b, we have result = $mulResult")
+
+    val devideResult = devide(a,b)
+    println("On Deviding $a and $b, we have result = $devideResult")
+
+
 }
 
-fun dateandTime() {
-    val dates = Date()
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    val formattedDate = dateFormat.format(dates)
-    println("Current Date and Time : $formattedDate")
+fun addition(a:Int, b:Int):Int {
+    return a + b
+}
+
+fun subtract(a: Int, b: Int):Int {
+    return a - b
+}
+
+fun multiply(a: Int, b:Int):Int {
+    return a * b
+}
+
+fun devide(a: Int, b: Int):Int {
+    return a / b
 }
