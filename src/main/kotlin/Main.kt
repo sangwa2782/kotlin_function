31a4864df37adb2816040360af3af6e205525b8a
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-15 19:58:03 +0530

    check if a given number is even or odd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.util.Date
 import java.util.Scanner
 
 
-// Write a Kotlin program to perform addition, subtraction, multiplication and division of two numbers.
+// Write a Kotlin program to check if a given number is even or odd.
 
 fun main () {
     val s = Scanner(System.`in`)
@@ -11,36 +11,14 @@ fun main () {
     print("Enter First digit : ")
     val a = s.nextInt()
 
-    print("Enter Second digit : ")
-    val b = s.nextInt()
-
-    val addResult = addition(a,b)
-    println("On Adding $a and $b, we have result = $addResult")
-
-    val subResult = subtract(a,b)
-    println("On Subtracting $a and $b, we have result = $subResult")
-
-    val mulResult = multiply(a,b)
-    println("On Multiplying $a and $b, we have result = $mulResult")
-
-    val devideResult = devide(a,b)
-    println("On Deviding $a and $b, we have result = $devideResult")
-
+    checkOddEven(a)
 
 }
 
-fun addition(a:Int, b:Int):Int {
-    return a + b
-}
-
-fun subtract(a: Int, b: Int):Int {
-    return a - b
-}
-
-fun multiply(a: Int, b:Int):Int {
-    return a * b
-}
-
-fun devide(a: Int, b: Int):Int {
-    return a / b
+fun checkOddEven(a:Int) {
+    if (a % 2 == 0) {
+        println("Your number is Even.")
+    } else {
+        println("Your number is odd.")
+    }
 }
