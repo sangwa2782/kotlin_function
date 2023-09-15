ea8b3c87b7220d6956676d403cec7f155f554b9c
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-15 20:26:54 +0530

    maximum and minimum of three numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.util.Date
 import java.util.Scanner
 
 
-// Write a Kotlin program to check if a given number is even or odd.
+// Write a Kotlin program to find the maximum and minimum of three numbers.
 
 fun main () {
     val s = Scanner(System.`in`)
@@ -11,14 +11,46 @@ fun main () {
     print("Enter First digit : ")
     val a = s.nextInt()
 
-    checkOddEven(a)
+    print("Enter Second digit : ")
+    val b = s.nextInt()
+
+    print("Enter Third digit : ")
+    val c = s.nextInt()
+
+
+    checkMax(a,b,c)
 
 }
 
-fun checkOddEven(a:Int) {
-    if (a % 2 == 0) {
-        println("Your number is Even.")
-    } else {
-        println("Your number is odd.")
+
+
+fun checkMax(a:Int, b:Int, c:Int) {
+
+   when {
+       a > b && a > c -> println("$a is Maximum among $a , $b and $c")
+       b > a && b > c -> println("$b is Maximum among $a , $b and $c")
+       else -> println("$c is Maximum among $a , $b and $c")
     }
+
+    when {
+        a < b && a < c -> println("$a is Minimum among $a , $b and $c")
+        b < a && b < c -> println("$b is Minimum among $a , $b and $c")
+        else -> println("$c is Minimum among $a , $b and $c")
+    }
+
+    val maximum = maxOf(a,b,c)
+    val minimum = minOf(a,b,c)
+
+    println("Maximum number among three numbers is : $maximum")
+    println("Minimum number among three numbers is : $minimum")
+
+//    using if-else condition
+//    if (a >= b && a >= c)
+//        println("$a is the largest number.")
+//    else if (b >= a && b >= c)
+//        println("$b is the largest number.")
+//    else
+//        println("$c is the largest number.")
+
+
 }
