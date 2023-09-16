92f0e3ea378dc44924942c11691e6dd374b68f7d
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-16 20:27:00 +0530

    Display Characters from A to Z using loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,23 +2,15 @@
 import java.util.*
 
 
-// Kotlin Program to Find LCM of two Numbers
+// Kotlin Program to Display Characters from A to Z using loop
 
 fun main() {
 
-    val a = 36
-    var b = 72
-    var gcd = 1
+   val a = 'A'
 
-    var i = 1
-
-    while (i < a && i < b) {
-        if (a % i == 0 && b % i == 0)
-            gcd = i
-            ++i
+    for (i in a..'Z') {
+        print("$i, ")
     }
-    val lcm = a*b/gcd
-    println("L.C.M of $a and $b is $lcm")
 }
 
 
