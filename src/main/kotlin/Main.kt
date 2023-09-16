cafaca9e1569631e56f60cc5c71c5b33f769b2f1
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-16 20:02:46 +0530

    Find Factorial of a Number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,20 +7,20 @@ import java.util.*
 fun main() {
 
     val a = 10
+    var b = 0
+    var c = 1
 
-    factorial(a)
+    println("Febonacci series of $a is : ")
 
-}
-
-fun factorial(a: Int) {
-    var b= 1
-
-    println("Table of $a is = ")
     for (i in 1..a) {
-        println("$a * $i = ${a*i}")
+        print("$b + ")
+        var sum = b + c
+        b = c
+        c = sum
     }
 
 }
+
 
 
 
