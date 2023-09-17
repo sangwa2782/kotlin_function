53bf758e18133fe58548de140ff4850cc7dabbab
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-17 13:31:08 +0530

    Display Factors of a Number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,25 +2,19 @@
 import java.util.*
 
 
-// Check Armstrong Number for 3 digit number
+// Kotlin Program to Display Factors of a Number
 
 fun main() {
+    val a = 10
+    var b = 0
 
-    var a = 154
-    var original = a
-    var result = 0
-
-    while (original != 0) {
-        var digit = original % 10
-        result += Math.pow(digit.toDouble(), 3.0).toInt()
-        original /= 10
+    for (i in 1..a) {
+        if (a%i==0) {
+            ++b
+        }
     }
+    print("$b")
 
-    if (a == result) {
-        println("$a is armstrong")
-    } else {
-        println("$a is not a prime number.")
-    }
 }
 
 
