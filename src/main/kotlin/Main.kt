7291293be611f0d1a510c3769f52fdb1a7451694
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-22 21:30:16 +0530

    Kotlin Recursion function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,29 @@
 
 import java.util.*
 
-// Program for default and named argument in Kotlin
 
 
-// function with Default and named Arguments
-fun printCountryDetails(countryName: String="India",noOfStates: Int=30){
-    println("Country : $countryName")
-    println("No. of States in $countryName : $noOfStates")
+// Kotlin Recursion function
+fun main(args:Array<String>){
+
+    //Recursion function is a function which call itself continuously.
+    //This technique is called Recursion function
+
+    rec()
+
+
 }
 
+//Table using recursion function
 
-// Main function, Entry Point of Program
-fun main(args:Array<String>){
-    // Call function without parameter so
-    // default value passed to function
-    printCountryDetails()
-
-    // Call function with parameter value
-    printCountryDetails("America",51)
-
-    // call function with only one parameter,
-    // so for second argument default value
-    // of argument will be used
-    printCountryDetails("Australia")
-
-    // call function with named argument,
-    // we write function argument name while
-    // calling function
-    printCountryDetails(countryName = "Japan",noOfStates = 15)
+val b = 5
+var count = 0
+fun rec() {
+    count++
+    if (count<=10) {
+        println("$b * $count = ${b*count}")
+        rec()
+    }
 }
 
 
