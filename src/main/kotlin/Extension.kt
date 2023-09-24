c50093661a7645373989fd444f85ad54743c29e1
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-24 16:35:14 +0530

    //kotlin Scope Function ( With )

--- src/main/kotlin/Extension.kt
+++ src/main/kotlin/Extension.kt
@@ -1,41 +1,29 @@
 
-//kotlin Scope Function ( Run )
+//kotlin Scope Function ( With )
 
 
 
 class Person {
-    var name:String? = null
-    var age:Int = 0
+    var name:String? = "PersonName"
+    var age:Int = 20
 }
 
 
 
 fun main() {
-    //code using apply scope()
-    //it returns the same object after applying
-    val person = Person().apply {
-        name="PersonName"
-        age = 26
-    }
+    val person = Person()
 
+    //Ordinary code
+    println(person.name)
+    println(person.age)
 
-    //like with, let returns last statement as lambdas
-    //You should use let when you want to perform a single
-    //action on an object
-    //
-    person.name?.let {
-        println(it.reversed())
-        println(it.length)
-    }
-
-
-    //when you want to perform multiple operation on the object and then
-    //return a result based on those actions
-    //run requirs return value
-    person?.run {
+    //code using with scope()
+    var returnFromWith : String = with(person) {
         println(name)
         println(age)
+        "last statement will return as lambda result"
     }
+    println(returnFromWith)
 
 }
 
