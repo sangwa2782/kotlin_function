505443d47bd86484e6ccd49d688ed0cfaf6491ac
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-24 11:57:44 +0530

    kotlin Extension Function(Array exchange)

--- src/main/kotlin/Extension.kt
+++ src/main/kotlin/Extension.kt
@@ -1,9 +1,19 @@
 
-//kotlin Extension Function(Find Charecter)
+//kotlin Extension Function(Array exchange)
 
-
-fun String.findChar(index:Int):Char = this[index]
 
 fun main() {
-    println("Ajay".findChar(1))
+    val list = mutableListOf(12,34,45,21)
+    println(list.swap(0,2))
+}
+
+fun MutableList<Int>?.swap(first:Int, second:Int):Any {
+    if (this == null) {
+        return "null"
+    } else {
+        var temp = this[first]
+        this[first] = this[second]
+        this[second] = temp
+        return this
+    }
 }
