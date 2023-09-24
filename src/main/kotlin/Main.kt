92e4b0ef66819672b47b65e87ceab924c2a2f3a4
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-24 10:15:34 +0530

    Kotlin Default Argument function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,40 +3,10 @@ import java.util.*
 
 
 
-// Kotlin Recursion function
-fun main(args:Array<String>){
-
-    //Recursion function is a function which call itself continuously.
-    //This technique is called Recursion function
-
-    rec()
-
-
+// Kotlin Default Argument function
+fun main () {
+    var result = add(4)
+    println(result)
 }
 
-//Table using recursion function
-
-val b = 12
-var count = 0
-fun rec() {
-    count++
-    if (count<=10) {
-        println("$b * $count = ${b*count}")
-        rec()
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fun add(a:Int, b:Int = 6) : Int = a+b
