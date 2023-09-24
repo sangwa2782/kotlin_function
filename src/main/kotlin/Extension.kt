abe82fbdad3719e18c5d263a997ae3a985147fe7
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-24 11:48:25 +0530

    kotlin Extension Function(Find Charecter)

--- src/main/kotlin/Extension.kt
+++ src/main/kotlin/Extension.kt
@@ -1,21 +1,9 @@
 
-//kotlin Extension Function
-class ExtensionRactangle(val length: Int, val width: Int) {
-
-    fun area(): Int = length * width
-
-}
-
-fun ExtensionRactangle.Perimeter (): Int {
-    var a = length + width
-    var b = 2*a
-    return b
-}
+//kotlin Extension Function(Find Charecter)
 
 
+fun String.findChar(index:Int):Char = this[index]
 
 fun main() {
-    val ractangle = ExtensionRactangle(5,9)
-
-    println(ractangle.Perimeter())
+    println("Ajay".findChar(1))
 }
