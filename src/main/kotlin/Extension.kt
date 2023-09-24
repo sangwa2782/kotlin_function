dca823317249cc80fa5b9f738a70dbe7507da415
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-09-24 16:23:31 +0530

    kotlin Scope Function ( Let )

--- src/main/kotlin/Extension.kt
+++ src/main/kotlin/Extension.kt
@@ -1,21 +1,36 @@
 
-//kotlin Higher Order Function
+//kotlin Scope Function ( Let )
+
+
+
+class Person {
+    var name:String? = null
+    var age:Int = 0
+}
+
 
 
 fun main() {
-    var sm:(a:Int, b:Int) -> Int = ::sum
-    println(sm(3,7))
-    calculator(5,15, ::sum)
+    //code using apply scope()
+    //it returns the same object after applying
+    val person = Person().apply {
+        name="PersonName"
+        age = 26
+    }
 
+
+    //like with, let returns last statement as lambdas
+    //You should use let when you want to perform a single
+    //action on an object
+    //
+    person.name?.let {
+        println(it.reversed())
+        println(it.length)
+    }
 
 }
 
 
-fun sum (a:Int, b:Int) : Int {
-    return a+b
-}
 
-fun calculator(a:Int, b:Int, sum:(Int, Int) -> Int) {
-    val result = sum(a,b)
-    println(result)
-}
+
+
